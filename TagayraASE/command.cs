73453ce83ba51948c10017b4c0338ea379f6db98
Ishra Banu <ishra166@gmail.com>
2023-11-14 05:14:02 +0545

    added XML DOC

--- TagayraASE/command.cs
+++ TagayraASE/command.cs
@@ -11,16 +11,31 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace TagayraASE
 {
+    
     public class Command : Shape
     {
         Boolean give = false;
         int xaxis2, yaxis2;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Command"/> class.
+        /// </summary>
+        /// <param name="g">The graphics object used for drawing.</param>
+        /// <param name="pen">The pen used for drawing outlines.</param>
+        /// <param name="positionx">The initial x-coordinate of the drawing position.</param>
+        /// <param name="positiony">The initial y-coordinate of the drawing position.</param>
+        /// <param name="brush">The brush used for filling shapes.</param>
         public Command(Graphics g, Pen pen, int positionx, int positiony, Brush brush) : base(g, pen, positionx, positiony,brush)
         {
 
         }
 
+        /// <summary>
+        /// Draws a line from the current position to the specified coordinates.
+        /// </summary>
+        /// <param name="command">The Command object.</param>
+        /// <param name="xaxis2">The x-coordinate to draw the line to.</param>
+        /// <param name="yaxis2">The y-coordinate to draw the line to.</param>
 
         public void DrawTo(Command command, int xaxis2, int yaxis2)
         {
@@ -29,6 +44,11 @@ namespace TagayraASE
 
         }
 
+        /// <summary>
+        /// Sets the fill state based on the provided command.
+        /// </summary>
+        /// <param name="command">The command ('on' or 'off').</param>
+        /// <returns>True if the fill state is 'on', false if 'off'.</returns>
         public Boolean Fill(string command)
         {
 
@@ -47,6 +67,12 @@ namespace TagayraASE
             return give;
         }
 
+        /// <summary>
+        /// Sets the pen color based on the provided color name.
+        /// </summary>
+        /// <param name="getcolor">The color name.</param>
+        /// <param name="pen">The pen object to modify.</param>
+        /// <returns>The updated color of the pen.</returns>
         public Color PenColor(string getcolor, Pen pen)
         {
             if (getcolor.Equals("yellow"))
@@ -78,6 +104,12 @@ namespace TagayraASE
         }
 
 
+        /// <summary>
+        /// Draws a circle on the canvas.
+        /// </summary>
+        /// <param name="command">The Command object.</param>
+        /// <param name="onoroff">True to fill the circle, false to draw the outline.</param>
+        /// <param name="radius">The radius of the circle.</param>
         public void DrawCircle(Command command, Boolean onoroff, int radius)
         {
             if (onoroff.Equals(false))
@@ -95,6 +127,13 @@ namespace TagayraASE
 
         }
 
+        /// <summary>
+        /// Draws a rectangle on the canvas.
+        /// </summary>
+        /// <param name="command">The Command object.</param>
+        /// <param name="onandoff">True to fill the rectangle, false to draw the outline.</param>
+        /// <param name="height">The height of the rectangle.</param>
+        /// <param name="width">The width of the rectangle.</param>
         public void DrawRectangle(Command command, Boolean onandoff, int height, int width)
         {
             if (onandoff.Equals(true))
@@ -108,7 +147,13 @@ namespace TagayraASE
 
             }
         }
-
+        /// <summary>
+        /// Draws an equilateral triangle on the canvas.
+        /// </summary>
+        /// <param name="command">The Command object.</param>
+        /// <param name="onoroff">True to fill the triangle, false to draw the outline.</param>
+        /// <param name="sidelength">The side length of the triangle.</param>
+      
         public void DrawTriangle(Command command, Boolean onoroff, int sidelength)
         {
             if (onoroff.Equals(true))
