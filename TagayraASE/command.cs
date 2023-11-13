7681af20ecb1a886c3401051960516f08030674d
Ishra Banu <ishra166@gmail.com>
2023-11-14 02:46:26 +0545

    function that runs fill command line argument

--- TagayraASE/command.cs
+++ TagayraASE/command.cs
@@ -11,7 +11,7 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace TagayraASE
 {
-       public class Command : Shape
+    public class Command : Shape
     {
         Boolean give = false;
         int xaxis2, yaxis2;
@@ -22,15 +22,66 @@ namespace TagayraASE
         }
 
 
-        public void DrawToPosition(Command command, int xaxis2, int yaxis2)
+        public void DrawTo(Command command, int xaxis2, int yaxis2)
         {
             g = command.g;
             g.DrawLine(command.pen, command.positionx, command.positiony, xaxis2, yaxis2);
 
         }
 
+        public Boolean Fill(string command)
+        {
 
-      
-        
+            if (command.Equals("on"))   //IF ON
+            {
+                give = true;
 
-}
+
+            }
+            else if (command.Equals("off")) //ELSE IF OFF
+            {
+                give = false;
+
+            }
+
+            return give;
+        }
+
+        public Color PenColor(string getcolor, Pen pen)
+        {
+            if (getcolor.Equals("yellow"))
+            {
+                pen.Color = Color.Yellow;
+
+            }
+
+            else if (getcolor.Equals("red"))
+            {
+                pen.Color = Color.Red;
+
+
+
+            }
+            else if (getcolor.Equals("purple"))
+            {
+                pen.Color = Color.Purple;
+
+
+            }
+            else if (getcolor.Equals("orange"))
+            {
+                pen.Color = Color.Orange;
+
+
+            }
+            return pen.Color;
+        }
+
+
+    }
+
+
+
+}     
+
+
